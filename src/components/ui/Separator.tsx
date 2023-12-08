55be98cb368f96dd4221ec249b907aa82c1b1ab1
devbylanre <devbylanre>
2023-12-08 14:45:33 +0100

    added separator UI component

--- src/components/ui/Separator.tsx
+++ src/components/ui/Separator.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '../../utils/util';
 
-export const Separator = () => {
-  return <div>Separator</div>;
+const separatorVariants = cva('bg-zinc-200', {
+  variants: {
+    orientation: {
+      horizontal: 'w-full h-[1px] my-3',
+      vertical: 'w-[1px] h-full mx-3',
+    },
+  },
+  defaultVariants: {
+    orientation: 'horizontal',
+  },
+});
+
+interface SeparatorProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof separatorVariants> {}
+
+export const Separator = ({ className, orientation }: SeparatorProps) => {
+  return <div className={cn(separatorVariants({ orientation, className }))} />;
 };
--- src/pages/theme/separator/ThemeSeparator.tsx
+++ src/pages/theme/separator/ThemeSeparator.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { ThemeHeader } from '../Theme';
+import React, { useState } from 'react';
+import { ThemeCard, ThemeHeader, ThemeTab } from '../Theme';
 import { Text } from '../../../components/ui/Text';
+import { Separator } from '../../../components/ui/Separator';
+import { Button } from '../../../components/ui/Button';
 
 export const ThemeSeparator = () => {
   return (
@@ -14,6 +16,46 @@ export const ThemeSeparator = () => {
           Similique quisquam accusamus
         </Text>
       </ThemeHeader>
+
+      <Card />
+    </div>
+  );
+};
+
+const Card = () => {
+  const [tab, setTab] = useState<'preview' | 'code'>('preview');
+  return (
+    <div className='flex flex-col space-y-8'>
+      <ThemeTab
+        tab={tab}
+        setTab={setTab}
+      />
+
+      <ThemeCard tab={tab}>
+        <div className='flex flex-col space-y-5'>
+          <div className='flex flex-col space-y-1 w-96'>
+            <Text
+              as='h6'
+              weight={500}
+            >
+              Testing separator component
+            </Text>
+            <Text
+              as='p'
+              size='sm'
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              assumenda impedit eos itaque possimus! Ducimus, nam enim?
+            </Text>
+          </div>
+          <Separator />
+          <div className='inline-flex'>
+            <Button variant='outline'>First name</Button>
+            <Separator orientation='vertical' />
+            <Button variant='outline'>Last name</Button>
+          </div>
+        </div>
+      </ThemeCard>
     </div>
   );
 };
