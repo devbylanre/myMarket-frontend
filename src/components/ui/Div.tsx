0ae8d57776abfad8d289136f6ad37739700bc5b0
devbylanre <devbylanre>
2024-01-12 04:03:04 +0100

    added Div component

---
+++ src/components/ui/Div.tsx
@@ -0,0 +1,35 @@
+import { VariantProps, cva } from 'class-variance-authority';
+import React, { HTMLAttributes, forwardRef } from 'react';
+import { cn } from '../../utils/util';
+
+const divVariants = cva('', {
+  variants: {
+    layout: {
+      grid: 'grid',
+      flex: 'flex',
+      block: 'block',
+      'inline-flex': 'inline-flex',
+      'inline-block': 'inline-block',
+    },
+  },
+
+  defaultVariants: {
+    layout: 'block',
+  },
+});
+
+interface Props
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof divVariants> {}
+
+export const Div = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { layout, className, ...rest } = props;
+
+  return (
+    <div
+      ref={ref}
+      className={cn(divVariants({ layout, className }))}
+      {...rest}
+    />
+  );
+});
